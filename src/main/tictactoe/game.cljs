625aca7e4afd0aba5d42a312f5a13cc8d0a3f998
gmsvalente <gustavomsvalente@gmail.com>
2024-09-18 09:46:32 -0300

    Create basic board layout

--- src/main/tictactoe/game.cljs
+++ src/main/tictactoe/game.cljs
@@ -3,13 +3,42 @@
 
 (defonce root (client/create-root (.getElementById js/document "root")))
 
-(defn game
+(defn cell-click
+  [cell-no]
+  (fn [el]
+    (println "cliked somewhere> " cell-no)
+    (println "cell-content= " (.-innerHTML (.-target el)))))
+
+
+(defn board
+  []
+  [:div.board
+   [:div.row
+    [:div.cell {:on-click (cell-click 1)} (str "X") ]
+    [:div.cell {:on-click (cell-click 2)} (str "O") ]
+    [:div.cell {:on-click (cell-click 3)} (str "X") ]]
+   [:div.row
+    [:div.cell {:on-click (cell-click 4)} (str "X") ]
+    [:div.cell {:on-click (cell-click 5)} (str "O") ]
+    [:div.cell {:on-click (cell-click 6)} (str "X") ]]
+   [:div.row
+    [:div.cell {:on-click (cell-click 7)} (str "X") ]
+    [:div.cell {:on-click (cell-click 8)} (str "O") ]
+    [:div.cell {:on-click (cell-click 9)} (str "X") ]]])
+
+(defn title-bar
   []
   [:div
-   [:h1 "TICTACTOE"]])
+   [:h1 "TicTacToe"]])
+
+(defn game
+  []
+  [:<>
+   [title-bar]
+   [board]])
 
 
-(defn ^:dev/load-after render!
+(defn ^:dev/after-load render!
   []
   (client/render root [game]))
 
